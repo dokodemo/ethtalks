a96e958897fecf4c95cea25087c43e93ab8527d9
truffle101 <truffle101@protonmail.com>
2018-04-10 17:17:28 +0800

    update logo

--- src/js/app.js
+++ src/js/app.js
@@ -40,7 +40,6 @@ App = {
     },
 
     fetchList: function() {
-        console.log("fetchList");
         App.contracts.ranking.methods.listRecords().call().then(function(results) {
             console.log(results);
             results.sort(function(l, r) {
@@ -50,6 +49,8 @@ App = {
                 if (bn1.lt(bn2)) return 1;
                 return 0;
             });
+            let maxValue = parseFloat(web3.utils.fromWei(results[0][1], "ether")) + 0.001;
+            $("#ethTalksValue").val(maxValue.toFixed(3));
 
             (async () => {
                 let html = "";
@@ -64,13 +65,10 @@ App = {
                         <div class="row text-center">
                             <div class="col-2">${(i + 1)}</div>
                             <div class="col"><a href="${record.link}" target="_blank">${record.name}</a></div>
-                            <div class="col-2">${web3.utils.fromWei(record.bid, 'ether')} ETH</div>                            
+                            <div class="col-2">${web3.utils.fromWei(record.bid, "ether")} ETH <a href="javascript:App.showSupport(${recordId})" id="supportTag" style="display:none">支持</a> </div>                            
                         </div>
-                        <div class="row justify-content-end mt-2 support" style="display:none;">
-                            <div class="col-4 input-group">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">支持</div>
-                                </div>
+                        <div class="row justify-content-end mt-2 support" id="support${recordId}" style="display:none;">
+                            <div class="col-3 input-group">
                                 <input type="number" class="form-control" id="ethTalksValue${recordId}" value="0.001" min="0.001" step="0.001">
                                 <div class="input-group-append">
                                     <div class="input-group-text">ETH</div>
@@ -87,8 +85,11 @@ App = {
         });
     },
 
+    showSupport: function(recordId) {
+        $(`#support${recordId}`).toggle();
+    },
+
     supportRecord: function(recordId) {
-        console.log(recordId);
         var value = $(`#ethTalksValue${recordId}`).val();
         web3.eth.getAccounts().then(function(accounts) {
             if (accounts.length > 0) {
@@ -119,89 +120,107 @@ $(function() {
     $("#noWeb3").hide();
     $("#noWeb3Mobile").hide();
 
-    $(window).on("load", function() {
-        var isMobile = false;
-        if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
-        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
-        
-        try {
-            if (web3) {
-                web3.eth.getAccounts(function(err, accounts){
-                    if (err != null) {
-                        alert("发生错误：" + err);
-                    } if (accounts.length == 0) {
-                        $("#metamaskLocked").show();
-                    } else {
-                        console.log("当前以太坊账号是：" + accounts[0]);
-                        $("#joinForm").show();
-                    }
-                });
-            }
-        } catch(e) {
-            if (isMobile == true) {
-                $("#noWeb3Mobile").show();
-            } else {
-                $("#noWeb3").show();
-            }
+    var isMobile = false;
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
+    
+    try {
+        if (web3) {
+            web3.eth.getAccounts(function(err, accounts){
+                if (err != null) {
+                    alert("发生错误：" + err);
+                } if (accounts.length == 0) {
+                    $("#metamaskLocked").show();
+                } else {
+                    console.log("当前以太坊账号是：" + accounts[0]);
+                    $("#joinForm").show();
+                }
+            });
+        }
+    } catch(e) {
+        if (isMobile == true) {
+            $("#noWeb3Mobile").show();
+        } else {
+            $("#noWeb3").show();
+        }
+    }
+
+    $("#joinButton").click(function(e) {
+        $("#panel").toggle();
+        e.preventDefault();
+    });
+
+    App.init();
+
+    $("#ethTalksValue").blur(function() {
+        var num = parseFloat($(this).val());
+        if (isNaN(num) || num < 0.001) {
+            num = 0.001;
+        }
+        var cleanNum = num.toFixed(3);
+        $(this).val(cleanNum);
+    });
+
+    $("#bidButton").click(function(e) {
+        e.preventDefault();
+
+        var name = $("#ethTalksName").val();
+        var link = $("#ethTalksLink").val();
+        var value = $("#ethTalksValue").val();
+        if (name.length <= 0) {
+            $("#modalAlertContent").text("请输入文案");
+            $("#modalAlert").modal();
+            return;
+        }
+        if (name.length > 20) {
+            $("#modalAlertContent").text("文案不能超过20个字符");
+            $("#modalAlert").modal();
+            return;
+        }
+        if (link.length <= 0) {
+            $("#modalAlertContent").text("请输入链接");
+            $("#modalAlert").modal();
+            return;
+        }
+        if (link.length > 50) {
+            $("#modalAlertContent").text("链接不能超过50个字符");
+            $("#modalAlert").modal();
+            return;
         }
 
-        $("#joinButton").click(function(e) {
-            $("#panel").slideToggle();
-            e.preventDefault();
-        });
-
-        App.init();
-
-        $("#ethTalksValue").blur(function() {
-            var num = parseFloat($(this).val());
-            if (isNaN(num) || num < 0.001) {
-                num = 0.001;
-            }
-            var cleanNum = num.toFixed(3);
-            $(this).val(cleanNum);
-        });
-
-        $("#bidButton").click(function(e) {
-            var name = $("#ethTalksName").val();
-            var link = $("#ethTalksLink").val();
-            var value = $("#ethTalksValue").val();
-
-            web3.eth.getAccounts().then(function(accounts) {
-                if (accounts.length > 0) {
-                    var account = accounts[0];
-                    App.contracts.ranking.methods.createRecord(name, link).send({from: account, value: web3.utils.toWei(value, "ether")})
-                    .then(function(receipt) {
-                        App.fetchList();
-                    });
-                } else {
-                    alert("Your metamask is locked!");
-                }
-                
-            });
-
-            e.preventDefault();
-        });
-
-        $("#withdrawButton").click(function(event) {
-            event.preventDefault();
-
-            console.log("withdraw");
-            web3.eth.getAccounts().then(function(accounts) {
+        web3.eth.getAccounts().then(function(accounts) {
+            if (accounts.length > 0) {
                 var account = accounts[0];
-                App.contracts.ranking.methods.withdraw().send({from: account}).then(function(receipt) {
-                    console.log(receipt);
-                }).catch(function(error) {
-                    console.log(error);
+                App.contracts.ranking.methods.createRecord(name, link).send({from: account, value: web3.utils.toWei(value, "ether")})
+                .then(function(receipt) {
+                    App.fetchList();
                 });
+            } else {
+                alert("请解锁 MetaMask。");
+            }
+            
+        });
+    });
+
+    $("#withdrawButton").click(function(event) {
+        event.preventDefault();
+
+        console.log("withdraw");
+        web3.eth.getAccounts().then(function(accounts) {
+            var account = accounts[0];
+            App.contracts.ranking.methods.withdraw().send({from: account}).then(function(receipt) {
+                console.log(receipt);
+            }).catch(function(error) {
+                console.log(error);
             });
         });
+    });
 
-        $(document).on("mouseenter", ".record", function() {            
-            $(this).find(".support").show();
-        });
-        
-        $(document).on("mouseleave", ".record", function() {
-            $(this).find(".support").hide();
-        });
+    $(document).on("mouseenter", ".record", function() {            
+        $(this).find("#supportTag").show();
+    });
+    
+    $(document).on("mouseleave", ".record", function() {
+        $(this).find("#supportTag").hide();
     });
 });
